6d8b212098317f53b3558482fe90937fab768dd4
hermsale <hermsale@gmail.com>
2025-09-25 19:28:14 -0300

    primer push

---
+++ Automovil.py
@@ -0,0 +1,10 @@
+ruedas = 4
+
+class Automovil:
+        def __init__ (self, color, marca, aceleracion, velocidad, ruedas=4):
+                self.ruedas = ruedas
+                self.color = color
+                self.marca = marca
+                self.aceleracion = aceleracion
+                self.velocidad = velocidad
+
